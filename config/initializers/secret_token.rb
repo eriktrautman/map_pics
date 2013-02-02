6ab60e8553cad053cb1303fd6022bc608f8f6bc9
Ryan Sepassi <rsepassi@gmail.com>
2013-02-02 14:47:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapPics::Application.config.secret_token = 'af53d84540e888f3069503a22b38b046e6f2c0c580865e93616f5fae8551191f5b69c81f56c3a7c4916ce37efee5e40deafc59e14eefe589a90e2ed5f36949e3'
